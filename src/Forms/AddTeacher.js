eb2ae5484c18d892d53aaadc264acaa4626210ea
Ketha <SK064731@cerner.net>
2020-03-29 19:52:39 +0530

    adding css

--- src/Forms/AddTeacher.js
+++ src/Forms/AddTeacher.js
@@ -3,31 +3,62 @@ import React from "react"
 import './addteacher.css';
 // --- This is the part to add
 
-import { Form,Button, Navbar,Nav,NavDropdown } from 'react-bootstrap'
+import { Form,Button, Navbar,Nav,NavDropdown,Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default class AddTeacher extends React.Component {
 	render() {
 		return (
 		<div id = "chai">
 			<Form>
-  <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>FirstName/Surname</Form.Label>
-    <Form.Control type="First Name" placeholder="First Name/Surname" />
-    <Form.Label>LastName</Form.Label>
-    <Form.Control type="Last Name" placeholder="LastName" />
-    <Form.Label>MobileNumber</Form.Label>
-    <Form.Control type="MobileNumber" placeholder="Mobile Number" />
-    <Form.Label>e-mail</Form.Label>
-    <Form.Control type="e-mail" placeholder="nam@xyz.com" />
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Description</Form.Label>
-  
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlInput2">
-  <Form.Label>AddImage(OPTIONAL)</Form.Label>
-    <Form.Control type="AddImage" placeholder="Upload Image" />
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
     </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" value="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
 </Form>
       
 </div>
